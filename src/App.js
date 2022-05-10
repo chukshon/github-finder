bdd81a291a52cd9b9d8684c21a9ec117d579b15a
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-05-10 16:58:44 +0100

    All pages created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Footer, Navbar } from './components/layouts'
+import { Home, About, NotFound } from './pages'
+
 export default function App() {
   return (
-    <div className='bg-purple-500'>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-      <button className='btn'>Click</button>
-    </div>
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/notFound' element={<NotFound />} />
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer></Footer>
+      </div>
+    </Router>
   )
 }
---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+import About from "./About";
+import Home from "./Home";
+import NotFound from "./NotFound";
+
+
+export {
+    About, Home, NotFound
+}
