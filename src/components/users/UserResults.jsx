0c54b8bbae2ffa8337fc6fc87c75a8542f2bfcac
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-05-10 20:40:44 +0100

    Created userItem and userResult components

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import Spinner from '../layouts/Spinner'
+import UserItem from './UserItem'
 
 const UserResults = () => {
   const [users, setUsers] = useState([])
@@ -15,18 +16,17 @@ const UserResults = () => {
         Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
       },
     })
-
     const data = await response.json()
     setUsers(data)
     setisLoading(false)
   }
 
-  if (!isLoading) {
+   if (!isLoading) {
     return (
-      <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md-grid-cols-2'>
-        {users.map((user) => {
-          return <h3>{user.login}</h3>
-        })}
+      <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
       </div>
     )
   } else {
